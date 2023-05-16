779bf2e2c40287563ab4201f30ebfebf2e0a3492
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-05-15 22:08:31 -0700

    Step 3 - Verify Axios is working

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const setup = async () => {
     console.log("Hello World")
+    const result = await axios.get("http://pokeapi.co/api/v2/pokemon?limit=810")
+    const pokemons = result.data.results
+    console.log(pokemons)
 
 };
 
