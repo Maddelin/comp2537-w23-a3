ca8775064f2cd5ac0250a2e93a8d7b3f1eb0e28f
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-05-15 22:19:39 -0700

    Step 4.2 Pokemons Grid - Create a pokemon card for each pokemon

--- index.js
+++ index.js
@@ -6,16 +6,22 @@ async function displayPokemon(pokemonData, firstIndex, lastIndex) {
     for (let i = 0; i < slicedPokemons.length; i++) {
         const pokemon = slicedPokemons[i];
         const pokemonResult = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
-        console.log(pokemonResult.data);
+        // console.log(pokemonResult.data);
 
+        // Step 4.2 Pokemons Grid - Create a pokemon card for each pokemon
         $('#main').append(`
-        <div class="card text-center" style="width: 18rem;">
-            <img class="card-img-top" 
-            src=
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonResult.data.id}.png" alt="Pokemon official artwork">
-            <div class="card-body">
-                <h5 class="card-title">${pokemon.name.toUpperCase()}</h5>
-                `)
+            <div class="card text-center" style="width: 18rem;">
+                <img class="card-img-top" 
+                src=
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonResult.data.id}.png" alt="Pokemon official artwork">
+                <div class="card-body">
+                    <h5 class="card-title">${pokemon.name.toUpperCase()}</h5>
+
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal${pokemon.name}">
+                    Show more
+                </button>
+        `)
     };
 }
 
